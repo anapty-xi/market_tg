f8b3e1b8d8ddd4d7679f41658a570d757e12e54b
anapty-xi <mironovn2006@gmail.com>
2026-03-25 16:40:36 +0300

    fix/order_usecases_fix

--- backend/apps/market/usecases/order/order_usecases.py
+++ backend/apps/market/usecases/order/order_usecases.py
@@ -1,3 +1,4 @@
+from apps.market.entities.order import Order
 from apps.market.usecases.order.order_usecases_protocol import OrderProtocol
 
 
@@ -8,17 +9,23 @@ class Base:
 
 class CreateOrder(Base):
     async def execute(self, tg_id: int, address: str, client_full_name: str) -> None:
-        order_id = await self.inf.create_order(tg_id)
+        order_id = await self.inf.create_order(tg_id, address, client_full_name)
         await self.inf.create_order_items(tg_id, order_id)
 
 
 class ChangeStatus(Base):
-    async def execute(self, id: int, status: str) -> None:
-        if status not in ["Оплачен", "Неоплачен", "В пути", "Выполнен"]:
+    async def execute(self, order_id: int, status: str) -> None:
+        stat_map = {
+            "Оплачен": "paid",
+            "Неоплачен": "unpaid",
+            "В пути": "on_the_way",
+            "Выполнен": "done",
+        }
+        if status not in stat_map:
             raise ValueError("invalid status")
-        await self.inf.change_status(id, status)
+        await self.inf.change_status(id, order_id, stat_map[status])
 
 
 class GetAllActiveOrders(Base):
-    async def execute(self) -> dict:
+    async def execute(self) -> list[Order]:
         return await self.inf.get_all_active_orders()
